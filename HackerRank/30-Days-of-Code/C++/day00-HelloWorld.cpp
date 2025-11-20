f5aca21cd8b76bd611cafce871fe612cdf436207
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-11-20 21:44:14 +0300

    added HackerRank

---
+++ HackerRank/30-Days-of-Code/C++/day00-HelloWorld.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int main() {
+    string input_string;
+    getline(cin,input_string);
+    cout << "Hello, World." << endl;
+    cout << input_string << endl;
+       
+    return 0;
+    //Sample output
+    /*Hello, World. 
+    Welcome to 30 Days of Code!*/
+}
---
+++ HackerRank/30-Days-of-Code/Java/day00-HelloWorld.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+	public static void main(String[] args) {
+        
+		Scanner scan = new Scanner(System.in); 
+		
+		
+		String inputString = scan.nextLine(); 
+
+
+		scan.close(); 
+      
+		
+		System.out.println("Hello, World.");
+        System.out.println(inputString);
+        //Sample output
+        /*Hello, World. 
+        Welcome to 30 Days of Code!*/
+	}
+}
