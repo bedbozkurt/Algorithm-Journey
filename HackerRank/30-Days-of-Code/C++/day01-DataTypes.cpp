21bc3a5caebae7a0e99edd6b58dc15f975d279a0
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-11-21 20:48:51 +0300

    day01

---
+++ HackerRank/30-Days-of-Code/C++/day01-DataTypes.cpp
@@ -0,0 +1,23 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int i=4;
+    double d=4.0;
+    string s="HackerRank ";
+    int i2;
+    double d2;
+    string s2;
+    string s3;
+    cin>>i2;
+    cin>>d2;
+    getline(cin,s2);
+    getline(cin,s3);
+    double d3=d+d2;
+    cout<<i+i2<<endl;
+    cout<<fixed<<setprecision(1)<<d3<<endl;
+    cout<<s+s3;  
+    return 0;
+}
---
+++ HackerRank/30-Days-of-Code/Java/day01-DataTypes.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+        int i2=scan.nextInt();
+        double d2=scan.nextDouble();
+        String s2=scan.nextLine();
+        String s3=scan.nextLine();
+        System.out.println(i+i2);
+        System.out.println(d2+d);
+        System.out.print(s+s3);
+
+       
+        scan.close();
+    }
+}
