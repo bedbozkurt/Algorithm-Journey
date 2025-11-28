cd8f9ecbf0955d24586060b47b5b264e89e8afee
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-11-28 21:54:51 +0300

    day08

---
+++ HackerRank/30-Days-of-Code/Java/day08-DictionariesAndMaps.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+//Question Link : https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem?isFullScreen=true
+class Solution {
+    public static void main(String []arg) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
+        
+        for(int i = 0; i < n; i++){
+            String name = scan.next();
+            int phone = scan.nextInt();
+            phoneBook.put(name, phone);
+        }
+        while(scan.hasNext()){
+            String s = scan.next();
+            if(phoneBook.containsKey(s)) {
+                System.out.println(s + "=" + phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        scan.close();
+    }
+}
