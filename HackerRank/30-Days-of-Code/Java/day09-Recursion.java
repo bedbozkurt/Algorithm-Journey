b8b54fb13a1e820e10a5ea1ace07de8ac7d16c66
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-11-29 20:48:52 +0300

    day09

---
+++ HackerRank/30-Days-of-Code/Java/day09-Recursion.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    //Question Link : https://www.hackerrank.com/challenges/30-recursion/problem?isFullScreen=true
+
+    public static int factorial(int n) {
+    if(n<=1){
+        return 1;
+    }
+    
+    return n*factorial(n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int n=scan.nextInt();
+        int result =factorial(n);
+        System.out.println(result);
+        
+        scan.close();
+    }
+
+}
+
+
