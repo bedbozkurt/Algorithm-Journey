6e3ed3d11c5fc6855186589e1fd15a76551f377b
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-04 23:15:49 +0300

    day14

---
+++ HackerRank/30-Days-of-Code/Java/day14-Scope.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+// Question Link : https://www.hackerrank.com/challenges/30-scope/problem?isFullScreen=true
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+    Difference(int[] elements){
+        this.elements=elements;
+    }
+    void computeDifference(){
+        int min = elements[0];
+        int max = elements[0];
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] < min) {
+                min = elements[i];
+            }
+            if (elements[i] > max) {
+                max = elements[i];
+            }
+        }
+        
+        maximumDifference = Math.abs(max - min);
+    }
+    
+} 
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
