a4e9be76ff4aee927f5c0fadb4589b2de7f60f3b
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-06 20:34:07 +0300

    day16

---
+++ HackerRank/30-Days-of-Code/C++/day16-StringToInteger.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <exception>
+using namespace std;
+
+// QuestionLink : https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
+
+int main()
+{
+    string S;
+    getline(cin, S);
+    
+    try{
+        int stringToInt =stoi(S);
+        cout << stringToInt;
+    }
+    catch(exception e){
+        cout << "Bad String";
+    }
+
+    return 0;
+}
