89579eef200ff744479ef4df9e4edd9aef649b94
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-07 20:53:14 +0300

    day17

---
+++ HackerRank/30-Days-of-Code/Java/day17-MoreExceptions.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+// Question Link : https://www.hackerrank.com/challenges/30-more-exceptions/problem?isFullScreen=true
+
+class Calculator{
+   public int power(int n, int p) throws Exception{
+        if(n<0 || p<0 ){
+            throw new Exception("n and p should be non-negative");
+        }
+        return (int) Math.pow(n, p);
+    }
+}
+class Solution{
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        while (t-- > 0) {
+        
+            int n = scan.nextInt();
+            int p = scan.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        scan.close();
+    }
+}
