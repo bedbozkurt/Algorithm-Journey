20734686b935d151ccac67d4109dab179620572e
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-15 20:19:31 +0300

    day25

---
+++ HackerRank/30-Days-of-Code/Java/day25- RunningTimeandComplexity.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+// Question Link : https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem?isFullScreen=true
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        for(int i = 0; i < T; i++){
+            int n = scan.nextInt();
+            
+            if (isPrime(n)) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not prime");
+            }
+        }
+        scan.close();
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false; 
+            }
+        }
+        
+        return true; 
+    }
+}
