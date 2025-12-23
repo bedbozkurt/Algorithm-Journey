fab90f65aaaed3f8a3618e93a702f36056747f62
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-23 20:48:01 +0300

    Two Pointer Valid Palindrome java code added

---
+++ NeetCode/02-Two-Pointers/ValidPalindrome.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Solution {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(isPalindrome(s));
+        scan.close(); 
+    }
+    public static boolean isPalindrome(String s) {
+      s = s.toLowerCase();
+      int low = 0;
+      int high = s.length() - 1;
+
+      while(low < high){
+        
+        if (!Character.isLetterOrDigit(s.charAt(low))) {
+            low++;
+            continue;
+        }
+        if (!Character.isLetterOrDigit(s.charAt(high))) {
+            high--;
+            continue;
+        }
+        if(s.charAt(low) != s.charAt(high)){
+            return false;
+        }
+        
+        low++;
+        high--;
+      }
+       
+      return true;
+    }
+}
