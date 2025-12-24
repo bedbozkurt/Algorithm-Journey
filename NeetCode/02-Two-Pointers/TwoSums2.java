56d93a88f9ed27b87a743548f6dc4cb704d383a0
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-24 21:31:11 +0300

    Two Pointer TwoSums2 java code added

---
+++ NeetCode/02-Two-Pointers/TwoSums2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length-1;
+        int currentsum = 0; 
+        while(left<right){
+            currentsum = numbers[left]+numbers[right];
+            if(currentsum == target){
+                return new int[]{left+1,right+1};
+            }
+            else if(currentsum > target){
+                right--;
+            }
+            else if(currentsum < target){
+                left++;
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);       
+        int n = scan.nextInt();
+        int[] nums = new int[n];
+        int target = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scan.nextInt();
+        }
+        int[] answer = twoSum(nums,target);
+        System.out.println(Arrays.toString(answer));
+        scan.close();
+    }
+}
