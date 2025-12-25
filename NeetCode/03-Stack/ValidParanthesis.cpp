5dc2f69a20204eac7d50f9aea7e863a8a808b71b
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-25 20:06:01 +0300

    Stack ValiDParanthesis cpp code added

--- NeetCode/03-Stack/ValidParanthesis.cpp
+++ NeetCode/03-Stack/ValidParanthesis.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <string>
+using namespace std;
 class Solution {
 public:
     bool isValid(string s) {
@@ -33,7 +34,7 @@ public:
         return st.empty();
     }
 };
-int main{
+int main(){
     string s;
     cin >> s;
     Solution sol;
