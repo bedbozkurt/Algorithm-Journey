4a4c8bbd64daaa7c586a217882fdb352ddc9bc7b
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-26 21:53:07 +0300

    Arrays-Hashing GroupAnagrams java code added

---
+++ NeetCode/01-Arrays-Hashing/GroupAnagrams.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map <String, List<String>> map = new HashMap<>();
+        
+        for(String s : strs){
+        char[] strsArray = s.toCharArray(); 
+        Arrays.sort(strsArray);
+        
+        String key = new String(strsArray);
+        if(!map.containsKey(key)){
+            map.put(key, new ArrayList<>());
+        }
+        map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] strs = new String[n];
+        for(int i = 0; i<n; i++){
+            strs[i] = scan.next();
+        }
+        List<List<String>> answer = groupAnagrams(strs);
+        System.out.println(answer);
+        scan.close();
+    }
+}
