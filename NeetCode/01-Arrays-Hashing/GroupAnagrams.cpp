fe55acdedae2fc00215bb55b8970153cc995fbb2
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2025-12-26 22:13:10 +0300

    Arrays-Hashing GroupAnagrams cpp code added

---
+++ NeetCode/01-Arrays-Hashing/GroupAnagrams.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class GroupAnagrams {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for(string s : strs){
+            string key = s; 
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for(auto x : mp){
+         result.push_back(x.second);
+        }
+        return result;
+    }
+};
+int main(){
+    int n;
+    cin >> n;
+    vector<string> strs;
+    for(int i =0; i<n;i++){
+        string temp;
+        cin >> temp;
+        strs.push_back(temp);
+    }
+    Solution sol;
+    vector<vector<string>> answer = sol.groupAnagrams(strs);
+    for(auto group : answer){
+        cout << "[ ";
+        for(auto word : group){
+            cout << word << " ";
+        }
+        cout << "]" << endl;
+    }
+    return 0;
+}
