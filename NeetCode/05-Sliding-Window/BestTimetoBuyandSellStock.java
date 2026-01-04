2b6393346199fe34882ea0c5964ce3aaba54194e
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-01-04 22:07:53 +0300

    Sliding-Window Best Time to Buy and Sell Stock java code added

---
+++ NeetCode/05-Sliding-Window/BestTimetoBuyandSellStock.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class BestTimetoBuyandSellStock {
+    public static int maxProfit(int[] prices) {
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+    for(int i=0; i<prices.length; i++) {
+        if(prices[i]<minPrice) {
+        minPrice=prices[i];
+        }
+        else if(prices[i]-minPrice > maxProfit) {
+        maxProfit=prices[i]-minPrice;
+    }
+    }
+        
+
+    return maxProfit;
+    }
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    int[] prices = new int[n];
+    for(int i=0; i<n; i++) {
+    prices[i]=scan.nextInt();
+    }
+    System.out.println(maxProfit(prices));
+}
+}
