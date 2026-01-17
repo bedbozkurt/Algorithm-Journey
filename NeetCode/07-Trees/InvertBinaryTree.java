377b23550536b32cbbb3272273364be738088ac7
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-01-17 12:47:40 +0300

    Trees InvertBinaryTree java code added

---
+++ NeetCode/07-Trees/InvertBinaryTree.java
@@ -0,0 +1,26 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
