c9ccae35a4d82e5ded2fd5e7177c716ed72dd468
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-01-27 14:19:12 +0300

    Backtracking CombinationSum java code added

---
+++ NeetCode/09-Backtracking/CombinationSum.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+    private void backtrack(List<List<Integer>>result, List<Integer>currentList, int[]candidates, int target, int start){
+        if(target == 0){
+            result.add(new ArrayList<>(currentList));
+            return;
+        }
+        if(target < 0){
+            return;
+        }
+        for(int i = start; i < candidates.length; i++){
+            currentList.add(candidates[i]);
+            backtrack(result,currentList,candidates,target-candidates[i],i);
+            currentList.remove(currentList.size()-1);
+        }
+    }
+}
