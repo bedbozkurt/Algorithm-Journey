bb9c8bdb10d3ec122c647863b7158a24c8bb85f0
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-02-09 19:20:59 +0300

    1D-Dp PalindromicSubstrings cpp code added

---
+++ NeetCode/12-1D-DP/PalindromicSubstrings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int count = 0;
+    int countSubstrings(string s) {
+        for(int i = 0; i< s.size(); i++){
+            helper(s, i, i);
+            helper(s, i, i + 1);
+        }
+        return count; 
+    }
+    void helper(const string& s, int left, int right){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            left--;
+            right++;
+            count++;
+        }
+    }
+};
