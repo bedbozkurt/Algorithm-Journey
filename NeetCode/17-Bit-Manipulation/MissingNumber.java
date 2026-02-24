6afd54167858859a7af63ec08f623af44d2d8d32
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-02-24 12:41:21 +0300

    Bit-Manipulation MissingNumber java code added

---
+++ NeetCode/17-Bit-Manipulation/MissingNumber.java
@@ -0,0 +1,11 @@
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int realSum = (n * (n+1))/2;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        return realSum - sum;
+    }
+}
