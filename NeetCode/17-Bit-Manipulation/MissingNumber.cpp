bcf4fb0e75782b02d9f895ca084b1c8820075fbb
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-02-24 12:42:18 +0300

    Bit-Manipulation MissingNumber cpp code added

---
+++ NeetCode/17-Bit-Manipulation/MissingNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int realSum = (n * (n+1))/2;
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+        }
+        return realSum - sum;
+    }
+};
