029386d9e92719061ec0a4b23a937fa003b1f48f
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-02-27 21:07:29 +0300

    Math-Geometry RotateImage java code added

---
+++ NeetCode/18-Math-Geometry/RotateImage.java
@@ -0,0 +1,24 @@
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+       int n = matrix.length;
+       for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+       } 
+       int left = 0;
+       int right = n - 1;
+       while(left < right){
+        for(int i = 0; i < n; i++){
+            int temp = matrix[i][left];
+            matrix[i][left] = matrix[i][right];
+            matrix[i][right] = temp;
+        }
+        left++;
+        right--;
+       }
+
+    }
+}
