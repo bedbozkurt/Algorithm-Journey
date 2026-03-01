3953be460320f719778f018390f348665374cf45
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-03-01 20:49:24 +0300

    Math-Geometry HappyNumber cpp code added

---
+++ NeetCode/18-Math-Geometry/HappyNumber.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = getSquareSum(n);
+        
+        while (fast != 1 && slow != fast) {
+            slow = getSquareSum(slow);
+            fast = getSquareSum(getSquareSum(fast));
+        }
+        
+        return fast == 1;
+    }
+    
+    int getSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+};
