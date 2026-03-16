d6e3be55ca1fb0fd8eba31c0289d846c9426f8a8
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-03-17 00:37:32 +0300

    Intervals MeetingRooms cpp code added

---
+++ NeetCode/13-Intervals/MeetingRooms.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals[i][0] < intervals[i-1][1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
