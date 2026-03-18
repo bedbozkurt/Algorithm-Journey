2c7af059a6cbcdbc0f191994b8d575c776407cc1
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-03-18 21:17:48 +0300

    Intervals MeetingRoomsII java code added

---
+++ NeetCode/13-Intervals/MeetingRoomsII.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MeetingRoomsII {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.add(intervals[0][1]);
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][0] >= heap.peek()){
+                heap.poll();
+            }
+            heap.add(intervals[i][1]);
+        }
+        return heap.size();
+    }
+}
