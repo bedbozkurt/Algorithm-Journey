645b55166a2b9ba2c0d01d979a5b5c35e62f59f2
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-03-20 19:53:08 +0300

    Greedy JumpGame java code added

---
+++ NeetCode/14-Greedy/JumpGame.java
@@ -0,0 +1,12 @@
+public class JumpGame{
+public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i > maxReach){
+                return false;
+            }
+            maxReach = Math.max(i+nums[i], maxReach);
+        }
+        return true;
+}
+}
