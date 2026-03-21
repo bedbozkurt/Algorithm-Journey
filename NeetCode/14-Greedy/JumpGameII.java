7181faa7947980a222995448d4a3d198a5242126
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-03-21 19:57:12 +0300

    Greedy JumpGameII java code added

---
+++ NeetCode/14-Greedy/JumpGameII.java
@@ -0,0 +1,15 @@
+public class JumpGameII {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+        for(int i = 0; i < nums.length -1; i++){
+            farthest = Math.max(i+nums[i], farthest);
+            if(i == currentEnd){
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+        return jumps;
+    }
+}
