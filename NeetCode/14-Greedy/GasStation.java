9b8350a51db89436a6a78d005c271f7bb8d9e02e
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-03-22 20:27:27 +0300

    Greedy GasStation java code added

---
+++ NeetCode/14-Greedy/GasStation.java
@@ -0,0 +1,19 @@
+public class  GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int currentGas = 0;
+        int startIndex = 0;
+        for(int i = 0; i < gas.length; i++){
+            totalGas += gas[i] - cost[i];
+            currentGas += gas[i] - cost[i];
+            if(currentGas < 0){
+                currentGas = 0;
+                startIndex = i+1;
+            }
+        }
+        if(totalGas < 0){
+            return -1;
+        }
+        return startIndex;
+    }
+}
