063ccd65b682446ec4761848417b23cb84ddb4a2
Bedirhan Bozkurt <bedirhan.bozkurt.dev@gmail.com>
2026-03-24 17:04:45 +0300

    Greedy MergeTripletstoFormTargetTriplet java code added

---
+++ NeetCode/14-Greedy/MergeTriplets.java
@@ -0,0 +1,26 @@
+public class MergeTriplets {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean first = false;
+        boolean second = false;
+        boolean third = false;
+        for(int i = 0; i < triplets.length; i++){
+            
+            if(triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2]){
+                continue;
+            }
+            if(triplets[i][0] == target[0]){
+                first = true;
+            }
+            if(triplets[i][1] == target[1]){
+                second = true;
+            }
+            if(triplets[i][2] == target[2]){
+                third = true;
+            }
+            if(first && second && third){
+                return true;
+            }
+        }
+        return false;
+    }
+}
